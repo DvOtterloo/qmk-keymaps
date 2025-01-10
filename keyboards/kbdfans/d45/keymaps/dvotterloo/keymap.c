eb4bbc49ef6fb8b8cac29b36455741b319235bb7
PC\Dennis <dennisvanotterloo@gmail.com>
2025-01-10 17:25:28 +0100

    format

--- keyboards/kbdfans/d45/keymaps/dvotterloo/keymap.c
+++ keyboards/kbdfans/d45/keymaps/dvotterloo/keymap.c
@@ -15,10 +15,14 @@
  */
 #include QMK_KEYBOARD_H
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-		[0] = LAYOUT( /* Base */
-		    KC_ESC, KC_TAB,    KC_Q,     KC_W,     KC_F,  KC_P,  KC_B,  KC_J,    KC_L,  KC_U,    KC_Y,     KC_SCLN,  KC_LBRC,  KC_RBRC, KC_BSLS, KC_DEL,
-		    KC_F2,  KC_BSPC,   KC_A,     KC_R,     KC_S,  KC_T,  KC_G,  KC_M,    KC_N,  KC_E,    KC_I,     KC_O,     KC_QUOT,  KC_ENT,  KC_HOME,
-		    KC_F3,  KC_LSFT,   KC_X,     KC_C,     KC_D,  KC_V,  KC_Z,  KC_K,    KC_H,  KC_COMM, KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,   KC_END,
-		    KC_F4,  KC_LCTL,   KC_LALT,            KC_SPC,       KC_ENT,         KC_RALT, KC_RCTL,                   KC_LEFT,  KC_DOWN, KC_RIGHT),
- };
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = 
+{
+
+	[0] = LAYOUT(
+		KC_ESC, 	KC_TAB,    	KC_Q,     	KC_W,     	KC_F,  		KC_P,  		KC_B,  		KC_J,    	KC_L,  		KC_U,    	KC_Y,     	KC_SCLN,  		KC_LBRC,  		KC_RBRC, 		KC_BSLS, 	KC_DEL,
+		KC_F2,  	KC_BSPC,   	KC_A,     	KC_R,     	KC_S,  		KC_T,  		KC_G,  		KC_M,    	KC_N,  		KC_E,    	KC_I,     	KC_O,     		KC_QUOT,  		KC_ENT,  					KC_HOME,
+		KC_F3,  	KC_LSFT,   	KC_X,     	KC_C,     	KC_D,  		KC_V,  		KC_Z,  		KC_K,    	KC_H,  		KC_COMM, 	KC_DOT,   	KC_SLSH,  		KC_RSFT,  		KC_UP,   					KC_END,
+		KC_F4,  	KC_LCTL,   	KC_LALT,  	          	KC_SPC,		       		KC_ENT,					KC_RALT, 	KC_RCTL,              	     			KC_LEFT,  		KC_DOWN, 		KC_RIGHT
+	),
+
+};
